583c20e86030dfb37970ab9d2fd21df1e99b4099
mika1407 <koneautomaatio@hotmail.com>
2021-05-02 21:35:40 +0300

    views added

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def display_laptops(request):
+    items = Laptop.objects.all()
+    context = {
+        'items' : items,
+        'header' : 'Laptops'
+    }
+    return render(request, 'index.html', context) # 3 arguments
+
+def display_desktops(request):
+    items = Desktop.objects.all()
+    context = {
+        'items' : items,
+        'header' : 'Desktops'
+    }
+    return render(request, 'index.html', context) # 3 arguments
+
+def display_mobiles(request):
+    items = Mobile.objects.all()
+    context = {
+        'items' : items,
+        'header' : 'Mobiles'
+    }
+    return render(request, 'index.html', context) # 3 arguments
