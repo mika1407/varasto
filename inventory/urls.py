633cb6f54be855a122c6acd12f94ccab2e35645d
mika1407 <koneautomaatio@hotmail.com>
2021-05-03 16:07:21 +0300

    add button added

--- inventory/urls.py
+++ inventory/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
 
     url(r'^display_laptop$', display_laptops, name='display_laptops'),
     url(r'^display_desktops$', display_desktops, name='display_desktops'),
-    url(r'^display_mobiles$', display_mobiles, name='display_mobiles')
+    url(r'^display_mobiles$', display_mobiles, name='display_mobiles'),
+
+    url(r'^add_laptop$', add_laptop, name="add_laptop"),
+    url(r'^add_desktop$', add_desktop, name="add_desktop"),
+    url(r'^add_mobile$', add_mobile, name="add_mobile")
 
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from .forms import *
 
 # Create your views here.
 def index(request):
@@ -28,3 +29,26 @@ def display_mobiles(request):
         'header' : 'Mobiles'
     }
     return render(request, 'index.html', context) # 3 arguments
+
+def add_item(request, cls):
+    if request.method == "POST":
+        form = cls(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+
+    else:
+        form = cls()
+        return render(request, 'add_new.html', {'form' : form})
+
+def add_laptop(request):
+    return add_item(request, LaptopForm)
+
+
+def add_desktop(request):
+    return add_item(request, DesktopForm)
+
+
+def add_mobile(request):
+    return add_item(request, MobileForm)
